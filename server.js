9b8c3808aa36839ebdd56ea4a957079972bf9bee
Carlos Araki <carlos.araki@ws-solutions.com>
2022-11-20 09:57:51 -0300

    feat: codigo funcional pegando salario de todos

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express(); 
+const https = require('https');
+
 
 const axios = require('axios');
 const cheerio = require("cheerio"); 
@@ -21,7 +23,6 @@ app.use(function(req, res, next){
  res.setHeader("Access-Control-Allow-Origin", "*");
  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
  res.setHeader("Access-Control-Allow-Headers", "content-type");
- res.setHeader("Content-Type", "application/json");
  res.setHeader("Access-Control-Allow-Credentials", true);
  next();
 });
@@ -65,47 +66,104 @@ app.get('/takeSalary', function(req, res){
           res.json(response);
         } else {
           var obj = JSON.parse(data);
+					var returnObj = [];
           obj.forEach(function(role) {
-            if (role != null) {
-                setTimeout(() => {
-                    axios.get('https://www.vagas.com.br/mapa-de-carreiras/servico/cargos/'+role[0], config)
-                    .then(function(response) {
-                        data = response.data
-                        let $ = cheerio.load(data)
-                        $(".mobileButton ").each((index, element) => { 
-                            let paginationURL = $(element).attr("href") 
-                            axios.get('https://www.vagas.com.br/mapa-de-carreiras/servico/'+paginationURL, config)
-                            .then(function(response) {
-                                data = response.data
-                                let $ = cheerio.load(data)
-                                $("dd .higher").each((index, element) => { 
-                                    let maiorSalario = $(element).text()
-                                    fs.writeFileSync('vagawithsalary.json', JSON.stringify(maiorSalario), function(err) {
-                                        if (err) {
-                                            var response = {status: 'falha', resultado: err};
-                                            res.json(response);
-                                        } else {
-                                            var response = {status: 'sucesso', resultado: 'Registro feito com sucesso'};
-                                            res.json(response);
-                                        }
-                                    });
-                                    var response = {status: 200, resultado: 'Registro feito com sucesso'};
-                                    res.json(response);
-                                })
-                            
-                            });
-                        })
-                },"1000")
-                   
-
-                  
-                });
-            }
+						if (role != null) {
+										axios.get('https://www.vagas.com.br/mapa-de-carreiras/servico/cargos/'+role[0], config)
+										.then(function(response) {
+												data = response.data
+												let $ = cheerio.load(data)
+												$(".mobileButton ").each((index, element) => { 
+														let paginationURL = $(element).attr("href") 
+														role.push('https://www.vagas.com.br/mapa-de-carreiras/servico/'+paginationURL)
+														returnObj.push(role);
+														fs.writeFileSync('vagawithurl.json', JSON.stringify(returnObj), function(err) {
+															if (err) {
+																res.json({'nok':'ok'});
+															} else {
+																res.json({'ok':'ok'});
+															}
+													});
+												})
+								});
+						}
           });
-
-
-          //res.json({});
+					res.json({'nok':'ok'});
+					
         }
       });
     
 });
+
+app.get('/takeSalaryV2', async function(req, res){
+	fs.readFile('vagasdb.json', 'utf8', async function(err, data){
+			if (err) {
+				var response = {status: 500, resultado: err};
+				res.json(response);
+			} 
+			else {
+				var obj = JSON.parse(data);
+				var returnObj = await takeurl(obj);
+				fs.writeFileSync('vagawithurl.json', JSON.stringify(returnObj), function(err) {
+					if (err) {
+						var response = {status: 500, resultado: err};
+						res.json({'nok':'nok'});
+					} 
+					else{
+						res.json({'ok':'ok'});
+					}
+				});
+			}
+		});
+	
+});
+
+
+async function delay(url) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+			console.log(url)
+      resolve('resolved');
+    }, 10);
+  });
+}
+
+async function takeurl($obj){
+	let returnObj = [];
+	for (let role of $obj) {
+		let delayRequest = await delay(role[0]);
+		let response = await  axios.get('https://www.vagas.com.br/mapa-de-carreiras/servico/cargos/'+role[0])
+		data = response.data
+		let $ = cheerio.load(data)
+		let paginationURL =''
+		$(".mobileButton ").each((index, element) => { 
+				paginationURL = $(element).attr("href") 
+		})
+		let orderData = 'https://www.vagas.com.br/mapa-de-carreiras/servico/'+paginationURL;
+
+		let responseData = await  axios.get(orderData)
+		data = responseData.data
+
+		let $2 = cheerio.load(data)
+		let salaryHigh ='N/I'
+		let salaryAverage ='N/I'
+		let salaryLower ='N/I'
+		$2(".higher .money").each((index, element) => { 
+			salaryHigh = $(element).html()
+		})
+		$2(".average .money").each((index, element) => { 
+			salaryAverage = $(element).html()
+		})
+		$2(".lower .money").each((index, element) => { 
+			salaryLower = $(element).html()
+		})
+		role.push(salaryHigh);
+		role.push(salaryAverage);
+		role.push(salaryLower);
+		role.push(orderData);
+		returnObj.push(role);
+	}
+	return returnObj
+
+}
+
